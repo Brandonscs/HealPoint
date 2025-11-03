da0f1b1ddefacd0184b71a2997b47e7d2aa62bed
Brandonscs <brandoncarvajal2002@gmail.com>
2025-11-03 00:06:35 -0500

    se actualiaron los archivos de backend y forntend

---
+++ Backend/healpoint/src/main/java/com/healpoint/HealpointApplication.java
@@ -0,0 +1,13 @@
+package com.healpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealpointApplication.class, args);
+	}
+
+}
---
+++ Backend/healpoint/src/test/java/com/healpoint/HealpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
